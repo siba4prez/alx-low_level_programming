9e7e86b359b3570a648e6852e8a79efd3c049e82
siba4prez <ritaopare@gmail.com>
2023-02-16 16:37:54 -0800

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *mail - entry point
+ *
+ * Return: 0 after printing the function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
